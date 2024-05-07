c4acfa7d8dc649069412c814756a0b902682c268
deboraayele <dayele2@umbc.edu>
2024-05-07 08:27:20 -0400

    My changes

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello IS147");
         System.out.println("Hello Deemah");
-        System.out.println("Hello Dayan");
     }
 
 }
