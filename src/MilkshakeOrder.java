b152061929c1f7474cba39fd0b0a150a6a578165
deboraayele <dayele2@umbc.edu>
2024-05-12 20:29:42 -0400

    Updated MilkshakeOrder

--- src/MilkshakeOrder.java
+++ src/MilkshakeOrder.java
@@ -8,7 +8,7 @@ public class MilkshakeOrder {
         this.scanner = scanner;
     }
 
-    public void orderMilkshake() {
+    public double orderMilkshake() {
         System.out.println("Our shop is known for our Strawberry Milkshakes!");
         System.out.println("Pick your flavor...");
         System.out.println("1.) Chocolate");
@@ -20,6 +20,7 @@ public class MilkshakeOrder {
         int choice = scanner.nextInt();
         scanner.nextLine(); // Consume the newline
 
+        double price = 4.99;
         String flavor;
         switch (choice) {
             case 1:
@@ -36,21 +37,28 @@ public class MilkshakeOrder {
                 break;
             default:
                 System.out.println("Invalid choice. Please choose a number between 1 and 4.");
-                return; // Early return on invalid choice
+                return 0;
         }
 
         System.out.print("Do you want whipped cream? (Yes or No): ");
         boolean whippedCream = scanner.next().equalsIgnoreCase("Yes");
+            if (whippedCream) {
+                price += 0.50;
+            }
         scanner.nextLine(); // Consume the newline
 
         System.out.print("Do you want a cherry on top? (Yes or No): ");
         boolean cherry = scanner.next().equalsIgnoreCase("Yes");
+            if (cherry){
+                price += 0.25;
+            }
         scanner.nextLine(); // Consume the newline
 
         System.out.println("Your order summary:");
         System.out.println("Flavor: " + flavor + " Milkshake");
         System.out.println("Whipped Cream: " + (whippedCream ? "Yes" : "No"));
         System.out.println("Cherry: " + (cherry ? "Yes" : "No"));
+            return price;
     }
 }
 
